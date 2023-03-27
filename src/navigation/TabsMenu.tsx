3ac80c81d07c7afece17adba874c0f0c649ad7ae
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-03-26 21:19:48 -0400

    adding icon in one element

--- src/navigation/TabsMenu.tsx
+++ src/navigation/TabsMenu.tsx
@@ -5,8 +5,13 @@ import {Tab1Screen} from '../screens/Tab1Screen';
 import {Tab2Screen} from '../screens/Tab2Screen';
 import {StackNavigator} from './StackNavigator';
 import {colors} from '../theme/appTheme';
+import {Text} from 'react-native';
 
 const Tab = createBottomTabNavigator();
+
+const iconB1 = (props: {focused: boolean; color: string; size: number}) => (
+  <Text style={{color: props.color}}>B1</Text>
+);
 
 export const TabsMenu = () => {
   return (
@@ -23,10 +28,13 @@ export const TabsMenu = () => {
           borderTopWidth: 0,
           elevation: 0,
         },
+        tabBarLabelStyle: {
+          fontSize: 13,
+        },
       }}>
       <Tab.Screen
         name="Tab1Screen"
-        options={{title: 'Tab1'}}
+        options={{title: 'Tab1', tabBarIcon: iconB1}}
         component={Tab1Screen}
       />
       <Tab.Screen
