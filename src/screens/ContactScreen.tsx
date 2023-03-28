f535e83b8d0fbc19cdad5c7d39b356227cc0d743
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-03-28 08:43:19 -0400

    sign in button has added

--- src/screens/ContactScreen.tsx
+++ src/screens/ContactScreen.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useContext} from 'react';
+import {View, Text, Button} from 'react-native';
+import {styles} from '../theme/appTheme';
+import {AuthContext} from '../context/AuthContext';
 
 export const ContactScreen = () => {
+  const {
+    signIn,
+    authState: {isLogging},
+  } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text>ContactScreen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>ContactScreen</Text>
+      {!isLogging && <Button title="Sign In" onPress={signIn} />}
     </View>
   );
 };
